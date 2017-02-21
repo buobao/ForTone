89e290754aa0882da1b1b6c103fcf98267497e94
buobao <320683buobao>
2017-02-21 23:01:30 +0800

    init push

---
+++ app/src/main/java/com/fortone/turman/fortone/MainActivity.java
@@ -0,0 +1,23 @@
+package com.fortone.turman.fortone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView.setOnClickListener(v->{
+            Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();
+        });
+    }
+}
